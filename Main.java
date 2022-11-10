98b9edf652b5b9e33385a44e910537c76c8b1adf
sota <s.nakagama.sys22@morijyobi.ac.jp>
2022-11-10 11:05:19 +0900

    1回目のコミット

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main{
+    public static void main(String[] args) {
+        System.out.println("êgí∑");
+        double tail=new java.util.Scanner(System.in).nextInt();
+        System.out.println("ëÃèd");
+        double weight=new java.util.Scanner(System.in).nextInt();
+        double BMI=weight/(tail*tail)*10000;
+        System.out.println(String.format("%.2f",BMI));
+    }
+}
