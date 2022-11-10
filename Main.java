72ca220078755c0d57a0540067834c6adb8057bc
sota <s.nakagama.sys22@morijyobi.ac.jp>
2022-11-10 11:25:49 +0900

    正しいコミット

--- Main.java
+++ Main.java
@@ -1,10 +1,12 @@
 public class Main{
     public static void main(String[] args) {
+	    for(int i=0; i<2; i++){
         System.out.println("g’·");
         double tail=new java.util.Scanner(System.in).nextInt();
         System.out.println("‘Ìd");
         double weight=new java.util.Scanner(System.in).nextInt();
         double BMI=weight/(tail*tail)*10000;
         System.out.println(String.format("%.2f",BMI));
+    }
     }
 }
